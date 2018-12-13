057cc3da5cc37466660f0e9df53894574bee56db
Augusto Julio <augusto.juliogec@gmail.com>
2018-12-13 13:39:36 -0200

    creating folder structure and defining provider entities

---
+++ src/main/java/model/Provider.java
@@ -0,0 +1,39 @@
+package model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="providers")
+public class Provider {
+
+@Id
+ private String id;
+ private String name;
+ private String address;
+
+ public String getId() {
+     return id;
+ }
+
+ public void setId(String id) {
+     this.id = id;
+ }
+
+ public String getName(){
+     return name;
+ }
+
+ public void setName(String name) {
+     this.name = name;
+ }
+
+ public String getAddress() {
+     return address;
+ }
+
+ public void setAddress(String address) {
+     this.address = address;
+ }
+
+}
+
